bec814089b2359ea84a465cb41ea7d238b88a84f
Laszlo Toth <laszlo.toth@nissin.com>
2022-09-10 12:55:52 +0200

    Add project files.

---
+++ PartyIn_L01/Controllers/HomeController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PartyIn_L01.Models;
+using System.Diagnostics;
+
+namespace PartyIn_L01.Controllers
+{
+    public class HomeController : Controller
+    {
+        //Első opció
+        //public string Index()
+        //{
+        //    return "Hello World";
+        //}
+
+        //Második opció
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Jó reggelt" :
+                "Kellemes délutánt";
+            //if(hour < 12)
+            //{ ViewBag.Greeting = "Jó reggelt"; }
+            //else
+            //{
+            //    ViewBag.Greeting = "Kellemes délutánt";
+            //}
+            return View("MyView");
+        }
+
+        [HttpGet]
+        public ViewResult InviteForm()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ViewResult InviteForm(GuestResponse guestResponse)
+        {
+            Repository.AddResponse(guestResponse);
+            return View("Thanks", guestResponse);
+        }
+
+        public ViewResult ListResponses()
+        {
+            return View(Repository.Responses
+                .Where(r => r.WillAttend == true));
+        }
+
+        //private readonly ILogger<HomeController> _logger;
+
+        //public HomeController(ILogger<HomeController> logger)
+        //{
+        //    _logger = logger;
+        //}
+
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        //public IActionResult Privacy()
+        //{
+        //    return View();
+        //}
+
+        //[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        //public IActionResult Error()
+        //{
+        //    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        //}
+    }
+}
---
+++ PartyIn_L01/Models/Repository.cs
@@ -0,0 +1,20 @@
+﻿namespace PartyIn_L01.Models
+{
+    public static class Repository
+    {
+        private static List<GuestResponse> respones = 
+            new List<GuestResponse>();
+
+        public static IEnumerable<GuestResponse> Responses
+        {
+            get { return respones; }
+        }
+
+        public static void AddResponse(GuestResponse guestResponse)
+        {
+            respones.Add(guestResponse);
+        }
+
+
+    }
+}
